048f09b19d91dc49eb990f1995bcd1bc22893087
bkrchristian <christianbosco.bosco67@gmail.com>
2022-09-04 10:48:55 -0300

    Adicionando main.py
    Incluir requirements.txt

---
+++ main.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+
+#Cria um reconhecedor
+r = sr.Recognizer()
+
+#Abrir microfone para captura
+with sr.Microphone() as source:
+    audio = r.listen(source) #Define microfone como fonte de audio
+
+    print(r.recognize_google(audio))
+    
